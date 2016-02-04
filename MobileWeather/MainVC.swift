0543a1dfab0a3e364e0d2d8d3b277efa4b9fd08c
alexbuessing <alexbuessing@gmail.com>
2016-02-04 16:28:59 -0500

    Final App

--- MobileWeather/MainVC.swift
+++ MobileWeather/MainVC.swift
@@ -364,7 +364,6 @@ class MainVC: UIViewController, CLLocationManagerDelegate {
     
     //Go to Search ViewController to add locations
     @IBAction func searchBtnPressed(sender: AnyObject) {
-//        locationManager.stopUpdatingLocation()
         self.performSegueWithIdentifier("searchSegue", sender: nil)
     }
     
